3628e58537156583a5ce064caba26dcb8221dd13
startupcode001 <startupcode001@gmail.com>
2021-12-30 14:26:07 -0800

    update

--- pe.py
+++ pe.py
@@ -27,7 +27,7 @@ def get_binary_file_downloader_html(bin_file, file_label='File'):
     with open(bin_file, 'rb') as f:
         data = f.read()
     bin_str = base64.b64encode(data).decode()
-    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">{file_label}</a>'
+    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{bin_file}">{file_label}</a>'
     return href
 
 @st.experimental_memo
